3ebda923f30158da3f512ccfe74a1750cad3c2b5
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-09-09 06:53:24 -0400

    commits

---
+++ dsa-trees/tree.js
@@ -0,0 +1,63 @@
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+  sumValues() {
+    if (!this.root) return 0;
+
+    let sum = 0;
+    function traverse(node) {
+      sum += node.val;
+      for (let child of node.children) {
+        traverse(child);
+      }
+    }
+
+    traverse(this.root);
+    return sum;
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+  countEvens() {
+    if (!this.root) return 0;
+
+    let count = 0;
+    function traverse(node) {
+      if (node.val % 2 === 0) count++;
+      for (let child of node.children) {
+        traverse(child);
+      }
+    }
+
+    traverse(this.root);
+    return count;
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+  numGreater(lowerBound) {
+    if (!this.root) return 0;
+
+    let count = 0;
+    function traverse(node) {
+      if (node.val > lowerBound) count++;
+      for (let child of node.children) {
+        traverse(child);
+      }
+    }
+
+    traverse(this.root);
+    return count;
+  }
+}
+
+module.exports = { Tree, TreeNode };
